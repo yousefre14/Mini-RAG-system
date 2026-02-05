970cae0e26e3e77eea18108d0e7e38eed6eb1746
yousefre14 <yousef.jo.reda14@gmail.com>
2026-02-05 04:24:25 +0200

    process controller added

---
+++ src/controllers/Process_Controller.py
@@ -0,0 +1,48 @@
+from .BaseController import BaseController
+from .ProjectController import ProjectController
+import os
+from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from models.enumerates.Processingenum import ProcessingEnum
+
+
+class ProcessController(BaseController):
+    def __init__(self, project_id: str):
+        self.project_id = project_id
+        super().__init__()
+        self.project_path = ProjectController().get_project_path(project_id=project_id)
+
+    def get_file_extension(self, file_id: str):
+        return os.path.splitext(file_id)[-1]
+    
+    def get_file_loader(self, file_id: str, file_path: str):
+        file_ext= self.get_file_extension(file_id=file_id)
+        if file_ext == ProcessingEnum.TXT.value:
+            return TextLoader(file_path, encoding = 'utf-8')
+        
+        if file_ext == ProcessingEnum.PDF.value:
+            return PyMuPDFLoader(file_path)
+        
+        return None
+
+    def get_file_content(self, file_id: str):
+        loader = self.get_file_loader(file_id=file_id, file_path=os.path.join(self.project_path, file_id))
+        return loader.load()
+    
+    def process_file_content(self, file_content: list, file_id:int, chunk_size: int=100, chunk_overlap: int=20):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap,length_function=len)
+
+    
+        file_content_Text=[
+            rec.page_content
+            for rec in file_content
+        ]
+
+        file_content_metadata=[
+            rec.metadata
+            for rec in file_content
+        ]
+
+        chunks= text_splitter.create_documents(file_content_Text, file_content_metadata)
+        return chunks
--- src/routes/data.py
+++ src/routes/data.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, Depends, UploadFile, status
 import os
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController, ProcessController
 import aiofiles
 from models.enumerates.responseEnamurates import ResponseSignals
 import logging
+from.schemas.data import processRequest
 
 logger = logging.getLogger("uvicor_errors")
 data_router = APIRouter(
@@ -46,3 +47,23 @@ async def upload_data(project_id: str, file: UploadFile,
             "file_id":'{file_id}'
         }
     )
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id:str, processRequest: processRequest):
+    file_id = processRequest.file_id
+    chunk_size = processRequest.chunk_size
+    overlap_size = processRequest.overlap_size
+
+
+
+    process_controller = ProcessController(project_id=project_id)
+    file_content = process_controller.get_file_content(file_id=file_id)
+    file_chunks = process_controller.process_file_content(file_content=file_content, file_id=file_id, chunk_size=chunk_size, chunk_overlap=overlap_size)
+
+    if file_chunks is None or len (file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"message": "Failed to process the file content."}
+        )
+    
+    return file_chunks
