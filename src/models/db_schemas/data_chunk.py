52b6099b53b96e71fa62165cbb68f25e235a3b6b
yousefre14 <yousef.jo.reda14@gmail.com>
2026-02-15 22:54:07 +0200

    mongo db schemas and models

--- src/models/db_schemas/data_chunk.py
+++ src/models/db_schemas/data_chunk.py
@@ -3,7 +3,7 @@ from bson.objectid import ObjectId
 from typing import Optional
 
 class DataChunk(BaseModel):
-    _id: Optional[ObjectId]
+    id: Optional[ObjectId]= Field(None, alias="_id")
     chunk_text: str = Field(..., min_length=1)
     chunk_metadata: dict = Field(...)
     chunk_order: int = Field(..., gt=0)
